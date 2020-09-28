04a4f4c581e09bcfd7b241b766d49d40d9457c0e
Bryan Yue <bryyue@microsoft.com>
2020-09-27 22:05:44 -0700

    implement logistic regression and add dataset

--- src/LogisticRegression/LogisticRegression.py
+++ src/LogisticRegression/LogisticRegression.py
@@ -1,43 +1,129 @@
 import numpy as np
+import pandas as pd
+import sys
+import matplotlib.pyplot as plt
 
 def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+    return 1 / (1 + np.exp(-1.0 * x))
 
 def augment_with_intercept(dataset):
     return np.insert(dataset, 0, np.ones(shape=(1, dataset.shape[1])))
 
-def get_initialzied_weights():
-    return np.zeros(shape=(x.shape[0], 1))
+def get_initialized_weights(size):
+    return np.zeros(shape=(size, 1))
 
 def predict(dataset, weights):
-    return np.rint(np.dot(dataset, weights))
+    return np.rint(get_activation(dataset, weights))
 
-def train(dataset, labels, iterations=100, learning_rate=0.5):
-    num_samples = labels.shape[1]
-    weights = get_initialzied_weights()
-    for i in range(iterations):
+def get_activation(dataset, weights):
+    return sigmoid(np.dot(weights.T, dataset))
+
+def get_cost(labels, activation):
+    sample_size = labels.shape[1]
+    return np.sum(labels * np.log(activation + sys.float_info.epsilon) + (1 - labels) * np.log(1 - activation + sys.float_info.epsilon)) / -sample_size
+
+def get_weight_gradient(labels, activation, dataset):
+    sample_size = labels.shape[1]
+    return np.dot(dataset, (activation - labels).T) / sample_size
+
+def train(dataset, labels, momentum=0.9, max_iterations=100000, learning_rate=0.1, epsilon=0.000001):
+    print("Training with momentum={0}, max_iterations={1}, learning_rate={2}, epsilon={3}".format(momentum, max_iterations, learning_rate, epsilon))
+    weights = get_initialized_weights(dataset.shape[0])
+    costs = []
+    prev_update_vector = None
+    prev_cost = None
+    iteration = 0
+
+    for _ in range(max_iterations):
         # forward propagation
-        activation = sigmoid(np.dot(weights.T, dataset))
-        cost = np.sum(labels * np.log(activation) + (1 - labels) * np.log(1 - activation)) / -num_samples
+        activation = get_activation(dataset, weights)
+        cost = get_cost(labels, activation)
+        costs.append(cost)
+
         # backward propagation
-        weight_gradient = np.dot(dataset, (activation - labels).T) / num_samples
-        weights = weights - learning_rate * weight_gradient
-        print("Iteration: {0}, Cost: {1}\n".format(i, cost))
-    return weights
+        update_vector = learning_rate * get_weight_gradient(labels, activation, dataset)
 
-def load_dataset():
-    raise NotImplementedError
+        # update weights
+        if prev_update_vector is not None:
+            update_vector += (momentum * prev_update_vector)
+        prev_update_vector = update_vector
+        weights = weights - update_vector
 
-def preprocess_data(dataset):
-    # augment dataset with row representing intercept with 1's
-    return np.insert(dataset, 0, np.ones(shape=(1, dataset.shape[1])))
+        # Check if we have converged
+        if prev_cost is not None and abs(prev_cost - cost) < epsilon:
+            print("Gradient descent has converted, exiting early after {0} iterations".format(iteration))
+            break
+
+        prev_cost = cost
+        iteration += 1
+    return weights, costs
+
+def load_data(file_path):
+    return pd.read_csv(file_path, sep=',', header=0)
+
+def normalize(feature):
+    return (feature - np.mean(feature)) / np.std(feature)
+
+def preprocess_data(df):
+    # ignore serial no in first column
+    df.drop(df.columns[0],axis=1,inplace=True)
+    features = df.columns.values[:-1]
+
+    # Normalize dataset
+    df['GRE Score'] = normalize(df['GRE Score'])
+    df['TOEFL Score'] = normalize(df['TOEFL Score'])
+    df['University Rating'] = normalize(df['University Rating'])
+    df['SOP'] = normalize(df['SOP'])
+    df['CGPA'] = normalize(df['CGPA'])
+
+    df_values = df.values.T
+    # augment dataset with first row representing intercept with 1's
+    dataset = np.insert(df_values, 0, np.ones(shape=(1, df_values.shape[1])), axis=0)
+    # last row of dataset will have labels
+    dataset[-1] = np.rint(dataset[-1])[np.newaxis]
+    return dataset, features
+
+def split_dataset(dataset, percent_train=60, shuffle=True):
+    if shuffle:
+        np.random.shuffle(dataset.T)
+
+    num_samples = dataset.shape[1]
+    num_train = num_samples * percent_train // 100
+
+    train_chunk = dataset[:,:num_train]
+    train_set = train_chunk[:-1]
+    train_labels = train_chunk[-1][np.newaxis]
+
+    test_chunk = dataset[:,num_train:]
+    test_set = test_chunk[:-1]
+    test_labels = test_chunk[-1][np.newaxis]
+
+    return train_set, train_labels, test_set, test_labels
+
+def get_accuracy(predictions, labels):
+    num_correct = (predictions[0] == labels[0]).sum()
+    accuracy = float(num_correct) / labels.shape[1]
+    return accuracy
 
 def main():
-    raw_data, labels = load_dataset()
-    dataset = preprocess_data(raw_data)
-    weights = train(dataset, labels)
-    predictions = predict(dataset, weights)
-    print("Predictions: {0}".format(predictions))
+    dataset, features = preprocess_data(load_data('admissionsData.txt'))
+
+    train_set, train_labels, test_set, test_labels = split_dataset(dataset)
+
+    print("Number of features: {0}".format(len(features)))
+    print("Train set size: {0}".format(train_set.shape[1]))
+    print("Test set size: {0}".format(test_set.shape[1]))
+
+    weights, costs = train(train_set, train_labels)
+
+    predictions = predict(test_set, weights)
+
+    accuracy = get_accuracy(predictions, test_labels)
+
+    print("Accuracy: {0}".format(accuracy))
+
+    plt.plot(costs)
+    plt.show()
 
 if __name__ == "__main__":
     main()
---
+++ src/LogisticRegression/README.md
@@ -0,0 +1,7 @@
+# Logistic Regression
+This script implements vectorized logistic regression and gradient descent with early stopping to avoid overfitting and momentum.
+We analyze graduate admissions data and predict whether a student will be admitted or not.
+Z-score normalization was utilized to speed up convergence.
+
+# Dataset Citation
+Mohan S Acharya, Asfia Armaan, Aneeta S Antony : A Comparison of Regression Models for Prediction of Graduate Admissions, IEEE International Conference on Computational Intelligence in Data Science 2019
